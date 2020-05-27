5765c72d2a3b53da51beebea327337bb7b4ff971
Allan Goncalves <allangoncalves@outlook.com>
2020-05-27 18:38:35 -0300

    dist path

--- src/server/index.js
+++ src/server/index.js
@@ -12,11 +12,11 @@ app.use("/api", router);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("dist"));
 
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile("dist/index.html");
   });
 }
 
