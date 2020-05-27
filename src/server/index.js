46b24ef1de6d4415911c433ad824677c7e97bf47
Allan Goncalves <allangoncalves@outlook.com>
2020-05-27 19:21:46 -0300

    without wildcard

--- src/server/index.js
+++ src/server/index.js
@@ -8,15 +8,15 @@ var router = require("./routers/index");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static("dist"));
 
 app.use("/api", router);
 
 if (process.env.NODE_ENV === "production") {
-  // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "dist/index.html"));
-  });
+  // // Handle React routing, return all requests to React app
+  // app.get("*", function(req, res) {
+  //   res.sendFile("dist/index.html");
+  // });
 }
 
 app.listen(process.env.PORT || 8080, () =>
