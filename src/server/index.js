f7dc6c6883cfbe1df5b1c4edae22ae5f0d813fda
Allan Goncalves <allangoncalves@outlook.com>
2020-05-27 18:28:34 -0300

    wildcard entry fild

--- src/server/index.js
+++ src/server/index.js
@@ -12,11 +12,11 @@ app.use("/api", router);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "dist")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
