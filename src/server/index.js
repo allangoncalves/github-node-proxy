1005dabd6e7e0c497abff56f87b5b00ad26a1bb6
Allan Goncalves <allangoncalves@outlook.com>
2020-05-27 18:14:08 -0300

    wildcard to fix react router

--- src/server/index.js
+++ src/server/index.js
@@ -7,9 +7,18 @@ var router = require("./routers/index");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("dist"));
 
 app.use("/api", router);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "dist")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 8080, () =>
   console.log(`Listening on port ${process.env.PORT || 8080}!`)
