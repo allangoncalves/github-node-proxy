1e0f88bd54c5d39576925233fc3024a1d3a86b00
Allan Goncalves <allangoncalves@outlook.com>
2020-05-27 22:57:04 -0300

    api root

--- src/client/services/api.js
+++ src/client/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const URL = process.env.BASE_API + "/api";
+
 const api = axios.create({
-  baseURL: process.env.BASE_API + "/api"
+  baseURL: URL
 });
 
 export default api;
